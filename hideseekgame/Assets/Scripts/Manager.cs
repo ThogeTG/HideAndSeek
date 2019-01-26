9e579e705b415924b62382829d7b2a950689f0b6
Siukola Henry TTK17SP <henrysiukola@kamk.fi>
2019-01-26 20:20:15 +0200

    gameplay stuff

--- hideseekgame/Assets/Scripts/Manager.cs
+++ hideseekgame/Assets/Scripts/Manager.cs
@@ -5,14 +5,18 @@ using UnityEngine.UI;
 
 public class Manager : MonoBehaviour
 {
+    public bool inputsEnabled;
+    public bool hideTime;
     public float timer;
-    public float timeLeft;
     public Text text;
     private bool finished;
+    public Camera[] hiderCams;
+    int round;
     // Start is called before the first frame update
     void Start()
     {
-        
+        inputsEnabled = true;
+        hideTime = true;
     }
 
     // Update is called once per frame
@@ -34,8 +38,35 @@ public class Manager : MonoBehaviour
     }
     public void Finish()
     {
-        finished = true;
-        text.color = Color.red;
+        round++;
+        if (round == 1)
+        {
+            finished = true;
+            text.color = Color.red;
+            foreach (Camera cam in hiderCams)
+            {
+                cam.clearFlags = CameraClearFlags.SolidColor;
+                cam.backgroundColor = Color.black;
+                cam.cullingMask = (1 << LayerMask.NameToLayer("nothing")) | (1 << LayerMask.NameToLayer("nothing"));
+                cam.fieldOfView = 0;
+
+            }
+            inputsEnabled = false;
+            StartCoroutine("look");
+        }
+        else if(round == 2)
+        {
+
+        }
+    }
+
+    IEnumerator look()
+    {
+        yield return new WaitForSeconds(3);
+        inputsEnabled = true;
+        text.color = Color.green;
+        timer = 100;
+        finished = false;
     }
 
 }
