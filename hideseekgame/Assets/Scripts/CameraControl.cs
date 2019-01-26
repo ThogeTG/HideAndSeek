be345de5c2fa1d1937cefc40f1826fbdc600d009
Siukola Henry TTK17SP <henrysiukola@kamk.fi>
2019-01-26 18:35:29 +0200

    xbox inputs

--- hideseekgame/Assets/Scripts/CameraControl.cs
+++ hideseekgame/Assets/Scripts/CameraControl.cs
@@ -8,21 +8,21 @@ public class CameraControl : MonoBehaviour
     Vector2 smoothV;
     public float sensitivity = 1;
     public float smoothing = 5;
-
+    Movement movement;
     GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
         player = this.transform.parent.gameObject;
-
+        movement = player.GetComponent<Movement>();
         //GetComponent<Camera>().cullingMask
     }
 
     // Update is called once per frame
     void Update()
     {
-        var middle = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        var middle = new Vector2(Input.GetAxisRaw("CamHMove"+movement.player), Input.GetAxisRaw("CamVMove"+movement.player));
 
         middle = Vector2.Scale(middle, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
         smoothV.x = Mathf.Lerp(smoothV.x, middle.x, 1f / smoothing);
--- hideseekgame/Assets/Scripts/Movement.cs
+++ hideseekgame/Assets/Scripts/Movement.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using XInputDotNetPure;
+
 
 public class Movement : MonoBehaviour
 {
+    public int player;
+    public PlayerIndex playerIndex;
     float realSpeed;
     public float walkSpeed = 10f;
     public float runSpeed = 20f;
     public float crawlSpeed = 2.5f;
 
+    
     float translation;
     float strafe;
 
@@ -40,6 +46,11 @@ public class Movement : MonoBehaviour
 
     public Text doorText;
 
+    GamePadState state;
+    GamePadState prevState;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -57,9 +68,19 @@ public class Movement : MonoBehaviour
         leftPeek = GetComponent<Animation>();
     }
 
+    private void Update()
+    {
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (!crouching)
+        {
+            originHeight = transform.position.y;
+        }
+
         Move();
 
         Crouch();
@@ -72,11 +93,11 @@ public class Movement : MonoBehaviour
 
     void Move()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (state.Buttons.RightShoulder == ButtonState.Pressed)
         {
             realSpeed = runSpeed;
         }
-        else if (Input.GetKey(KeyCode.C))
+        else if (state.Buttons.B == ButtonState.Pressed)
         {
             realSpeed = crawlSpeed;
         }
@@ -86,8 +107,8 @@ public class Movement : MonoBehaviour
         }
         
 
-        translation = Input.GetAxis("Vertical") * realSpeed;
-        strafe = Input.GetAxis("Horizontal") * realSpeed;
+        translation = Input.GetAxis("VMove" + player) * realSpeed;
+        strafe = Input.GetAxis("HMove" + player) * realSpeed;
 
         translation *= Time.deltaTime;
         strafe *= Time.deltaTime;
@@ -98,8 +119,7 @@ public class Movement : MonoBehaviour
     void Crouch()
     {
         Vector3 pos = transform.position;
-
-        if (Input.GetKey(KeyCode.C))
+        if (state.Buttons.B == ButtonState.Pressed /*&& prevState.Buttons.B == ButtonState.Released*/)
         {
             crouching = true;
         }
@@ -118,6 +138,7 @@ public class Movement : MonoBehaviour
             pos.y = Mathf.Lerp(transform.position.y, originHeight, .3f);
             transform.position = pos;
         }
+
     }
 
     private void OnTriggerStay(Collider other)
